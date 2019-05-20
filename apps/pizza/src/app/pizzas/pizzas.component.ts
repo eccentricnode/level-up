83269d4ef9b23b02632e4a6f778411bc81a07405
eccentricnode <shival.bigman@gmail.com>
2019-05-20 16:43:10 -0700

    incremental checkin

--- apps/pizza/src/app/pizzas/pizzas.component.ts
+++ apps/pizza/src/app/pizzas/pizzas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Observable } from 'rxjs';
+import { PizzaService, Pizza } from '@level/core-data';
 
 @Component({
   selector: 'level-pizzas',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pizzas.component.scss']
 })
 export class PizzasComponent implements OnInit {
+  pizzas$: Observable<Pizza[]>;
 
-  constructor() { }
+  pizzas: Pizza[];
+
+  constructor(
+    private pizzaService: PizzaService
+  ) { }
 
   ngOnInit() {
+    this.getPizzas();
+    this.pizzas$.subscribe(pizzas => this.pizzas = pizzas);
   }
 
+  getPizzas() {
+    this.pizzas$ = this.pizzaService.all();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log(event);
+    moveItemInArray(this.pizzas, event.previousIndex, event.currentIndex);
+  }
 }
