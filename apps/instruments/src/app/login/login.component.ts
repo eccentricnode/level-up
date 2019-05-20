a02ef147ec300831ff594e7d5116e72f9ed378fe
eccentricnode <shival.bigman@gmail.com>
2019-05-19 18:02:27 -0700

    set up login component

--- apps/instruments/src/app/login/login.component.ts
+++ apps/instruments/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, NgForm, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService, NotifyService } from '@level/core-data';
 
 @Component({
   selector: 'level-login',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  verifiedUser = { username: 'username', password: 'password' };
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private notifyService: NotifyService
+  ) { }
 
   ngOnInit() {
   }
 
+  login() {
+    const { username, password } = this.form.value;
+    const verifiedUser = this.verifiedUser.username === username &&
+      this.verifiedUser.password === password;
+  
+    if (this.form.valid && verifiedUser) {
+      this.router.navigateByUrl('/instruments');
+      this.authService.setToken(username);
+      this.notifyService.notify('Welcome ${username}!', 'Oh YEAH!');
+    }
+  }
+
+  validateField(control: string, directive: NgForm) {
+    return this.form.get(control).invalid && directive.submitted;
+  }
+
+  private initForm() {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])],
+    })
+  }
 }
