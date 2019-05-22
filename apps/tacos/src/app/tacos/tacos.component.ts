8bd2e5546058e8e76a4978d9edd0948177fce3b9
eccentricnode <shival.bigman@gmail.com>
2019-05-22 00:12:34 -0700

    incremental checkin

--- apps/tacos/src/app/tacos/tacos.component.ts
+++ apps/tacos/src/app/tacos/tacos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Observable } from 'rxjs';
+import { Taco, TacosService } from '@level/core-data';
 
 @Component({
   selector: 'level-tacos',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tacos.component.scss']
 })
 export class TacosComponent implements OnInit {
+  tacos$: Observable<Taco[]>;
+  tacos: Taco[];
 
-  constructor() { }
+  constructor(private tacosService: TacosService) { }
 
   ngOnInit() {
+    this.getTacos();
+    this.tacos$.subscribe(tacos => this.tacos = tacos);
   }
 
+  getTacos() {
+    this.tacos$ = this.tacosService.all();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.tacos, event.previousIndex, event.currentIndex);
+  }
 }
